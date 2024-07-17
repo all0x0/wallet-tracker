981f538a1a244e33892dc75d0e694e89576d16ec
winer328 <rexdev328@gmail.com>
2024-07-17 05:23:28 +0000

    initial-update

---
+++ src/constant.js
@@ -0,0 +1,30 @@
+module.exports = {
+    BOT_STATE: {
+        START: "start",
+        WELCOME: "welcome",
+        HELP: "help",
+        WALLET_MANAGE: "wallet_manage",
+        CREATE_WALLET :"create_wallet",
+        IMPORT_WALLET :"import_wallet",
+        WALLET_TRANSFER:"wallet_transfer",
+        REMOVE_WALLET:"remove_wallet",
+        BUY_SELL_SELECT: "buy_sell_select",
+        COPY_TRADE: "copy_trade",
+        RESULT: "result"
+    },
+    MESSAGE: {
+        COMMON_SUCCESS_MESSAGE: "Your action is excuted successfully.",
+        INVALID_COIN_ADDRESS: "The coin address entered is not valid. Reenter."
+    },
+    COPY_TRADE: {
+        CREATE_TRADE: "create_copy_trade",
+        PAUSE_ALL: "pause_all_trade",
+        ACTIVE_ALL: "active_all_trade",
+        UPDATE_TRADE: "update_copy_trade",
+        SAVE_TRANSACTION: "save_transaction",
+    },
+    GENERAL_ACTION: {
+        BACK: "back",
+        DOWNLOAD_WALLET_LIST: "download_wallet_list"
+    }
+}
